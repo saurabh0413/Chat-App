cae748c14070f9744793045c4a0374ce0608cfb0
saurabh <saurabhfarkade83@gmail.com>
2023-02-25 21:58:54 +0530

    basic-components-created

---
+++ chatapp/src/pages/Mainroutes.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Chatpage from "./Chatpage";
+import Login from "./Login";
+import Signup from "./Signup";
+
+const Mainroutes = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/chats" element={<Chatpage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default Mainroutes;
